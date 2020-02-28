db68b41908564a2d98f3c5e1f93f1edf18cd7e69
Simon <simonjturk@gmail.com>
2020-02-28 16:59:19 +1100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,17 @@ module.exports = {
     require('postcss-import')(),
     require('tailwindcss')('./src/css/tailwind.config.js'),
     require('autoprefixer'),
-    require('postcss-reporter')
+    require('@fullhuman/postcss-purgecss')({
+
+      // Specify the paths to all of the template files in your project 
+      content: [
+        './site/**/*.html'
+        // etc.
+      ],
+
+      // Include any special characters you're using in this regular expression
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }),
+    require('postcss-reporter'),
   ],
 };
