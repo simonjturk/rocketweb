5606ca701de1b496e3c6e6719902e0c6aa702edb
Simon <simonjturk@gmail.com>
2020-02-28 19:27:11 +1100

    moved icon files

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import')(),
-    require('tailwindcss')('./src/css/tailwind.config.js'),
+    require('tailwindcss')('./src/css/tailwind.config.js'), //I'm not sure if this is the best way to do this considering I have a theme.  Sure;y that should be doing this??
     require('autoprefixer'),
     require('@fullhuman/postcss-purgecss')({
 
